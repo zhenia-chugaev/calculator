b2e3957b4764fa3490bc039ec4e8fe4311122476
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-02-13 16:49:01 +0300

    Add app class

---
+++ src/js/App.js
@@ -0,0 +1,67 @@
+import Calculator from './Calculator';
+import InputCommand from './commands/InputCommand';
+import OperateCommand from './commands/OperateCommand';
+import MemoizeCommand from './commands/MemoizeCommand';
+import ClearCommand from './commands/ClearCommand';
+
+const commandsByButtonType = {
+  operand: InputCommand,
+  operation: OperateCommand,
+  memory: MemoizeCommand,
+  clear: ClearCommand,
+};
+
+export default class App {
+  calculator = new Calculator();
+
+  scoreboardElement = null;
+
+  handleClick = (e) => {
+    const target = e.target.closest('.calculator__button');
+    const { type, value } = target.dataset;
+    const Command = commandsByButtonType[type];
+    const command = new Command(value, this.calculator);
+    command.execute();
+    this.render();
+  };
+
+  init() {
+    this.scoreboardElement = document.querySelector('.calculator__scoreboard');
+    const calculatorMenu = document.querySelector('.calculator__menu');
+    calculatorMenu.addEventListener('click', this.handleClick);
+    this.render();
+  }
+
+  render() {
+    this.scoreboardElement.innerHTML = '';
+
+    const { state } = this.calculator;
+
+    if (state.memory.hasSavedValue()) {
+      const memoryElement = document.createElement('span');
+      memoryElement.classList.add('calculator__scoreboard-memory');
+      this.scoreboardElement.appendChild(memoryElement);
+    }
+
+    const scoreElement = document.createElement('p');
+    scoreElement.classList.add('calculator__scoreboard-score');
+
+    if (state.errorMessage) {
+      scoreElement.textContent = state.errorMessage;
+      scoreElement.classList.add('calculator__scoreboard-error');
+      this.scoreboardElement.appendChild(scoreElement);
+      return;
+    }
+
+    const [x = '', y = ''] = state.operands;
+    const sign = Calculator.signs[state.operation] || '';
+    const score = `${x}${sign}${y}`;
+
+    scoreElement.textContent = score;
+    scoreElement.classList.add(
+      score.length > 12 ? 'calculator__scoreboard-score_size_small' : null,
+    );
+
+    this.scoreboardElement.appendChild(scoreElement);
+  }
+}
