723f9512f899ae66c303e58bd2a2dabb82e8d917
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-03-28 20:10:57 +0300

    Add theme switcher

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import App from './js/App';
 
 const app = new App();
 app.init();
+
+const themeSwitcher = document.querySelector('.theme-switcher');
+
+themeSwitcher.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+});
