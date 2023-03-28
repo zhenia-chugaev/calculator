2e515a178c354da5f0187339ee92d89a34591fcc
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-03-28 20:03:09 +0300

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default {
+  entry: ['./src/index.js', './src/style.css'],
+  output: {
+    path: path.resolve(dirname, 'build/'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
